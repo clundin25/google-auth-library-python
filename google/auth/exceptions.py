c09a569daf7763c1263c73954d810f8578ef2d9a
Carl Lundin <clundin@google.com>
2022-07-26 23:49:10 +0000

    Add a property based retryable exception check.

--- google/auth/exceptions.py
+++ google/auth/exceptions.py
@@ -22,10 +22,26 @@ class GoogleAuthError(Exception):
 class TransportError(GoogleAuthError):
     """Used to indicate an error occurred during an HTTP request."""
 
+    def __init__(self, *args, retryable=False):
+        super(TransportError, self).__init__(*args)
+        self._retryable = retryable
+
+    @property
+    def retryable(self):
+        return self._retryable
+
 
 class RefreshError(GoogleAuthError):
     """Used to indicate that an refreshing the credentials' access token
     failed."""
+
+    def __init__(self, *args, retryable=False):
+        super(RefreshError, self).__init__(*args)
+        self._retryable = retryable
+
+    @property
+    def retryable(self):
+        return self._retryable
 
 
 class UserAccessTokenError(GoogleAuthError):
@@ -53,10 +69,15 @@ class OAuthError(GoogleAuthError):
 class ReauthFailError(RefreshError):
     """An exception for when reauth failed."""
 
-    def __init__(self, message=None):
+    def __init__(self, message=None, retryable=False):
         super(ReauthFailError, self).__init__(
             "Reauthentication failed. {0}".format(message)
         )
+        self._retryable = retryable
+
+    @property
+    def retryable(self):
+        return self._retryable
 
 
 class ReauthSamlChallengeFailError(ReauthFailError):
